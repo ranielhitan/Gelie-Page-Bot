90b43875cd11b481143b5b24a6d87b352aa28eb2
makabugto123 <139425756+makabugto123@users.noreply.github.com>
2024-10-31 22:31:22 +0800

    Add files via upload

---
+++ commands/shoti.js
@@ -0,0 +1,37 @@
+const axios = require('axios');
+//const { sendMessage } = require('../handles/sendMessage');
+
+module.exports = {
+name: 'shoti',
+description: 'Generate a random TikTok video.',
+author: 'Jerome',
+async execute(senderId, args, pageAccessToken, sendMessage) {
+try {
+const apiUrl = 'https://shoti.kenliejugarap.com/getvideo.php?apikey=shoti-52603bcda611603ad60ee87762f92cf70e06f96e6626a4d4b7b9f87f2887cd4876b8e78c6ce01c5095006a8d5aa172c76c66954d2faf6840249b64db723ff950470d3525fffe661f9969d8b818e2b5b9976651b426';
+const response = await axios.get(apiUrl);
+const videoUrl = response.data.videoDownloadLink;
+const title = response.data.title;
+const username = response.data.username;
+const tiktokUrl = response.data.tiktokUrl;
+
+  const message = Title: ${title}\nUsername: ${username}\nTikTok URL: ${tiktokUrl};
+  await sendMessage(senderId, { text: message }, pageAccessToken);
+
+  const videoMessage = {
+    attachment: {
+      type: 'video',
+      payload: {
+        url: videoUrl,
+      },
+    },
+  };
+  await sendMessage(senderId, videoMessage, pageAccessToken);
+} catch (error) {
+  console.error('Error:', error.message);
+  sendMessage(senderId, {
+    text: 'Sorry, there was an error generating the video. Please try again later.',
+  }, pageAccessToken);
+}
+
+},
+};
