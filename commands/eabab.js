637cc0bb3c321750cfcfb17654511ed84fa4502e
makabugto123 <139425756+makabugto123@users.noreply.github.com>
2024-11-01 00:21:47 +0800

    Create eabab.js

---
+++ commands/eabab.js
@@ -0,0 +1,39 @@
+
+const axios = require('axios');
+const { sendMessage } = require('../handles/sendMessage');
+
+module.exports = {
+name: 'eabab',
+usage: 'eabab',
+description: 'Generate a random girl video.',
+author: 'Jerome',
+async execute(senderId, args, pageAccessToken, sendMessage) {
+try {
+const apiUrl = 'https://hiroshi-api.onrender.com/video/eabab';
+const response = await axios.get(apiUrl);
+const videoUrl = response.data.link;
+const title = response.data.title;
+const username = response.data.username;
+const displayname = response.data.displayname;
+
+  const message = `Title: ${title}\nUsername: ${username}\nDisplay Name: ${displayname}`;
+  await sendMessage(senderId, { text: message }, pageAccessToken);
+
+  const videoMessage = {
+    attachment: {
+      type: 'video',
+      payload: {
+        url: videoUrl,
+      },
+    },
+  };
+  await sendMessage(senderId, videoMessage, pageAccessToken);
+} catch (error) {
+  console.error('Error:', error.message);
+  sendMessage(senderId, {
+    text: 'Sorry, there was an error generating the video. Please try again later.',
+  }, pageAccessToken);
+}
+
+},
+};
