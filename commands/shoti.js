d18c839f06c1b28dd77f1604a1d375f89356a1d1
makabugto123 <139425756+makabugto123@users.noreply.github.com>
2024-10-31 23:00:34 +0800

    Update shoti.js

--- commands/shoti.js
+++ commands/shoti.js
@@ -10,11 +10,11 @@ try {
 const apiUrl = 'https://shoti.kenliejugarap.com/getvideo.php?apikey=shoti-52603bcda611603ad60ee87762f92cf70e06f96e6626a4d4b7b9f87f2887cd4876b8e78c6ce01c5095006a8d5aa172c76c66954d2faf6840249b64db723ff950470d3525fffe661f9969d8b818e2b5b9976651b426';
 const response = await axios.get(apiUrl);
 const videoUrl = response.data.videoDownloadLink;
-const title = response.data.title;
-const username = response.data.username;
-const tiktokUrl = response.data.tiktokUrl;
+//const title = response.data.title;
+//const username = response.data.username;
+//const tiktokUrl = response.data.tiktokUrl;
 
-  const message = `Title: ${title}\nTikTok URL: ${tiktokUrl}`;
+  const message = `𝕯𝖔𝖜𝖓𝖑𝖔𝖆𝖉𝖎𝖓𝖌...`;
   await sendMessage(senderId, { text: message }, pageAccessToken);
 
   const videoMessage = {
