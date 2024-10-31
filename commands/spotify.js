ba46e5825571e258dbea0f30d0d62ba3bcf23c88
makabugto123 <139425756+makabugto123@users.noreply.github.com>
2024-11-01 01:03:46 +0800

    Update spotify.js

--- commands/spotify.js
+++ commands/spotify.js
@@ -8,6 +8,15 @@ module.exports = {
   author: 'coffee',
 
   async execute(senderId, args, pageAccessToken) {
+if (!args || args.length === 0) {
+      // Send message requesting a prompt if missing
+      await sendMessage(senderId, {
+        text: '❌ 𝗣𝗹𝗲𝗮𝘀𝗲 𝗽𝗿𝗼𝘃𝗶𝗱𝗲 𝘆𝗼𝘂𝗿 𝗽𝗿𝗼𝗺𝗽𝘁\n\n𝗘𝘅𝗮𝗺𝗽𝗹𝗲: Spotify Nang Dumating Ka.'
+      }, pageAccessToken);
+      return;  // Exit the function if no prompt is provided
+    }
+
+    
     try {
       const { data } = await axios.get(`https://hiroshi-api.onrender.com/tiktok/spotify?search=${encodeURIComponent(args.join(' '))}`);
       const link = data[0]?.download;
