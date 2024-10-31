3a4e3978375d88a68b7a35b6a5ef2c4ac4af1118
makabugto123 <139425756+makabugto123@users.noreply.github.com>
2024-11-01 07:28:36 +0800

    Add files via upload

---
+++ commands/riddle.js
@@ -0,0 +1,28 @@
+const axios = require('axios');
+const { sendMessage } = require('../handles/sendMessage');
+
+module.exports = {
+  name: 'ai',
+  description: 'Interact with ChronoAI',
+  usage: 'ai [your question]',
+  author: 'coffee',
+
+  async execute(senderId, args, pageAccessToken) {
+    const prompt = args.join(' ');
+    if (!prompt) return sendMessage(senderId, { text: "Usage: ai <question>" }, pageAccessToken);
+
+    try {
+      const apiUrl = 'https://ccproject10-df3227f754.onlitegix.com/api/randomriddle';
+const response = await axios.get(apiUrl);
+const riddle = response.data.riddle;
+const ans = response.data.answer;
+//const username = response.data.username;
+//const displayname = response.data.displayname;
+
+       const mes = `Riddle: ${riddle}\nAnswer: ${ans}`;
+  await sendMessage(senderId, { text: mes }, pageAccessToken);
+    } catch {
+      sendMessage(senderId, { text: 'There was an error generating the content. Please try again later.' }, pageAccessToken);
+    }
+  }
+};
