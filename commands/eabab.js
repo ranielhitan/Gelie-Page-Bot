74572aa4b28f218ac93ba603c032217c00220635
makabugto123 <139425756+makabugto123@users.noreply.github.com>
2024-11-01 00:30:22 +0800

    Update eabab.js

--- commands/eabab.js
+++ commands/eabab.js
@@ -16,7 +16,7 @@ const title = response.data.title;
 const username = response.data.username;
 const displayname = response.data.displayname;
 
-  const message = `Title: ${title}\nUsername: ${username}\nDisplay Name: ${displayname}\n\n​🇩​​🇴​​🇼​​🇳​​🇱​​🇴​​🇦​​🇩​​🇮​​🇳​​🇬​ ​🇵​​🇱​​🇪​​🇦​​🇸​​🇪​ ​🇼​​🇦​​🇮​​🇹​...
+  const message = `Title: ${title}\nUsername: ${username}\nDisplay Name: ${displayname}\n\n𝕯𝖔𝖜𝖓𝖑𝖔𝖆𝖉𝖎𝖓𝖌 𝕻𝖑𝖊𝖆𝖘𝖊 𝖂𝖆𝖎𝖙...
 `;
   await sendMessage(senderId, { text: message }, pageAccessToken);
 
