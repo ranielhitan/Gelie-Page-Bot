263a46d9659d783a925157611022ebf6f8a39dbb
makabugto123 <139425756+makabugto123@users.noreply.github.com>
2024-11-01 07:32:10 +0800

    Update riddle.js

--- commands/riddle.js
+++ commands/riddle.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 const { sendMessage } = require('../handles/sendMessage');
 
 module.exports = {
-  name: 'ai',
+  name: 'riddle',
   description: 'Interact with ChronoAI',
   usage: 'ai [your question]',
   author: 'coffee',
