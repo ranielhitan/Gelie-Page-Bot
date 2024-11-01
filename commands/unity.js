d1f22c21f6f4755417b7fd1e5e048bec33e9bd57
makabugto123 <139425756+makabugto123@users.noreply.github.com>
2024-11-02 06:41:14 +0800

    Create unity.js

---
+++ commands/unity.js
@@ -0,0 +1,23 @@
+const axios = require('axios');
+const { sendMessage } = require('../handles/sendMessage');
+
+module.exports = {
+  name: 'unity',
+  description: 'Get answer from unity ai',
+  usage: 'unity [question]',
+  author: 'coffee',
+
+  async execute(senderId, args, pageAccessToken) {
+
+    const prompt = args.join(' ');
+    if (!prompt) return sendMessage(senderId, { text: "Usage: unity <question>" }, pageAccessToken);
+
+    try {
+       const { data } = await axios.get(`https://api.kenliejugarap.com/unity/?question=${encodeURIComponent(prompt)}`);
+      
+      sendMessage(senderId, { text: data.extract }, pageAccessToken);
+    } catch {
+      sendMessage(senderId, { text: 'There was an error generating the content. Please try again later.' }, pageAccessToken);
+    }
+  }
+};
