7d80f31b7bc1561fdc579800b68125db79db0293
makabugto123 <139425756+makabugto123@users.noreply.github.com>
2024-11-04 07:44:16 +0800

    Create micgen.js

---
+++ commands/micgen.js
@@ -0,0 +1,26 @@
+const axios = require('axios');
+const { sendMessage } = require('../handles/sendMessage');
+
+module.exports = {
+  name: 'micgen',
+  description: 'Generate Microsoft Account',
+  usage: 'micgen <name>',
+  author: 'coffee',
+
+  async execute(senderId, args, pageAccessToken) {
+
+    try {
+      const apiUrl = 'https://joshweb.click/api/genmicro?name=${encodeURIComponent(prompt)}';
+const response = await axios.get(apiUrl);
+const user = response.data.email;
+const pass = response.data.password;
+//const username = response.data.username;
+//const displayname = response.data.displayname;
+
+       const mes = `Microsoft Account\nUsername: ${user}\nAnswer: ${pass}`;
+  await sendMessage(senderId, { text: mes }, pageAccessToken);
+    } catch {
+      sendMessage(senderId, { text: 'There was an error generating the content. Please try again later.' }, pageAccessToken);
+    }
+  }
+};
