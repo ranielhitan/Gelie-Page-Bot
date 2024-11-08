2940f2257dd118ae2acafac9acd01a6c90fa7efe
Bogiti <https://github.com/ranielhitan/Gelie-Page-Bot.git>
2024-11-08 21:32:21 +0800

    update aiv2.js

---
+++ commands/aiv2.js
@@ -0,0 +1,21 @@
+const axios = require('axios');
+const { sendMessage } = require('../handles/sendMessage');
+
+module.exports = {
+  name: 'ai',
+  description: 'Xao Ai',
+  usage: 'aiv2 [your Question or Message]',
+  author: 'Gelie',
+
+  async execute(senderId, args, pageAccessToken) {
+    const prompt = args.join(' ');
+    if (!prompt) return sendMessage(senderId, { text: "Usage: ai <question>" }, pageAccessToken);
+
+    try {
+      const { data: { result } } = await axios.get(`https://api.y2pheq.me/?q=${encodeURIComponent(prompt)}&UID=${senderId}`);
+      sendMessage(senderId, { text: result }, pageAccessToken);
+    } catch {
+      sendMessage(senderId, { text: 'There was an error generating the content. Please try again later.' }, pageAccessToken);
+    }
+  }
+};
