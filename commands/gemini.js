8efc8ef379b67998f4bfd89beb45d95845d62c37
Bogiti <https://github.com/ranielhitan/Gelie-Page-Bot.git>
2024-11-28 07:10:08 +0800

    update gemini.js

--- commands/gemini.js
+++ commands/gemini.js
@@ -11,7 +11,7 @@ module.exports = {
     if (!prompt) return sendMessage(senderId, { text: "Usage: gemini <your message>" }, pageAccessToken);
 
     try {
-      const { data } = await axios.get(`https://joshweb.click/gemini?prompt=${encodeURIComponent(prompt)}`);
+      const { data } = await axios.get(`https://api.kenliejugarap.com/blackbox-gpt4o/?text=${encodeURIComponent(prompt)}`);
       sendMessage(senderId, { text: data.gemini }, pageAccessToken);
     } catch {
       sendMessage(senderId, { text: 'Error generating response. Try again later.' }, pageAccessToken);
