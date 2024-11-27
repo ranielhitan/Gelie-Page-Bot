2cf99e7a6ec5d2c3e5022fecabe18af7020a69f8
ranielhitan <ranielhitan@gmail.com>
2024-11-27 17:59:08 +0800

    Update gpt4.js

--- commands/gpt4.js
+++ commands/gpt4.js
@@ -12,7 +12,7 @@ module.exports = {
     if (!prompt) return sendMessage(senderId, { text: "Usage: gpt4 <question>" }, pageAccessToken);
 
     try {
-      const { data: { result } } = await axios.get(`https://api.kenliejugarap.com/freegpt-openai/?question=Hello%20from%20api.kenliejugarap.com${encodeURIComponent(prompt)}&uid=${senderId}`);
+      const { data: { result } } = await axios.get(`https://api.kenliejugarap.com/freegpt-openai/?question=Hello%20from%20api.kenliejugarap.com${encodeURIComponent(prompt)}`);
       sendMessage(senderId, { text: result }, pageAccessToken);
     } catch {
       sendMessage(senderId, { text: 'There was an error generating the content. Please try again later.' }, pageAccessToken);
