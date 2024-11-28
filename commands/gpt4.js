18b7afc3ff9754461c05454dfae9452249c10bfe
Bogiti <https://github.com/ranielhitan/Gelie-Page-Bot.git>
2024-11-28 21:36:37 +0800

    update gpt4.js

--- commands/gpt4.js
+++ commands/gpt4.js
@@ -12,7 +12,7 @@ module.exports = {
     if (!prompt) return sendMessage(senderId, { text: "Usage: gpt4 <question>" }, pageAccessToken);
 
     try {
-      const { data: { result } } = await axios.get(`https://chatgpt-api-conversational.onrender.com/alice?prompt=hi${encodeURIComponent(prompt)}&uid=${senderId}`);
+      const { data: { result } } = await axios.get(`https://chatgpt-api-conversational.onrender.com/alice?prompt=${encodeURIComponent(prompt)}&uid=${senderId}`);
       sendMessage(senderId, { text: response }, pageAccessToken);
     } catch {
       sendMessage(senderId, { text: 'There was an error generating the content. Please try again later.' }, pageAccessToken);
